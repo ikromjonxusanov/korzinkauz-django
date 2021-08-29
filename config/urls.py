366065ca6ed83c86e46c66c5c2a3edae5fbacab8
Ikromjon <ikromjonkhusanov06@gmail.com>
2021-08-29 18:06:01 +0500

    pagination and filter added

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from config.views import ExampleView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,6 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('test/', ExampleView.as_view())
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
